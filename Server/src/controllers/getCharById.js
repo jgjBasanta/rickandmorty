f8aa33e83d482ab5c9ddb7fb3c565e0ddc772b23
Jose Basanta <jgj1989@gmail.com>
2023-05-24 14:54:36 -0400

    promesas en server

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const getCharById = (res, id)=>{
+    axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then((result)=> result.data)
+    .then(({ name, gender, origin, image, status, species })=>{
+        let character = {
+            id,
+            name,
+            gender,
+            origin,
+            image,
+            status,
+            species
+        }
+        res.writeHead(200, {"Content-type": "application/json"}).end(JSON.stringify(character));
+    })
+    .catch((error)=>res.writeHead(500, {"Content-type": "text/plain"}).end(error.message));
+};
+
+module.exports = {
+    getCharById
+}
